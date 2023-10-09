ac590c81226cfe13bc2647ad486f3871a264adcb
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-09 11:40:16 -0400

    implement GetConfig()

--- config/config.go
+++ config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	DiscordToken string `yaml:"discordtoken"`
+}
+
+func GetConfig() (Config, error) {
+	f, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return Config{}, fmt.Errorf("unable to read config file: %v", err)
+	}
+
+	c := Config{}
+	err = yaml.Unmarshal(f, &c)
+	if err != nil {
+		return Config{}, fmt.Errorf("unable to unmarshal config into struct: %v", err)
+	}
+
+	return c, nil
+}
